036eb2fa54ecc2b3cda6671cd1ae13a0d68ce568
Waron <warrenabayon@gmail.com>
2023-09-30 06:26:24 +0800

    finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(greetings, numRepeat) {
 
-};
+let addGreet = '';
+for (i = 0; i < numRepeat; i++){
+    addGreet += 'hey';  
+}
+
+return addGreet;
+
+}
 
 // Do not edit below this line
 module.exports = repeatString;
