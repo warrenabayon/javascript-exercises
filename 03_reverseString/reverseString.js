adf896e928f98fc5bedc8e7c6763ee3e3b4ab6d3
Waron <warrenabayon@gmail.com>
2023-10-01 09:10:08 +0800

    finished exercise 3: reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(textInput) {
 
-};
+    /*
+    return textInput.split('').reverse().join(''); /// method 1
+    */
+
+    //method 2
+    let reverseText = '';
+
+    for (i = textInput.length - 1; i >= 0; i--){
+        reverseText += textInput[i];
+    }
+
+    return reverseText; 
+
+}
 
 // Do not edit below this line
 module.exports = reverseString;
