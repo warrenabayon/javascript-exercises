fa88502fbbb61935a5f8eaf2dec22b1e7ccd9ef1
Waron <warrenabayon@gmail.com>
2023-10-05 06:59:21 +0800

    finished exercises 4 Remove From Array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
 const removeFromArray = function() {
 
+    const argumentArray = arguments[0];
+
+    for (i = 1; i < arguments.length; i++){
+        // const argumentValue = arguments[i]
+        //console.log(argumentValue);// check the argument value
+        const argumentIndex = arguments[0].indexOf(arguments[i]);
+        //console.log(argumentIndex)// check the index
+
+        //check if the argument is not existing
+        if (argumentIndex !== -1) { // -1 represent not found
+            const argumentArray = arguments[0].splice(argumentIndex, 1);
+        }
+        
+    }
+    
+    return argumentArray;
+
 };
 
 // Do not edit below this line
