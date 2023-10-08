612a0a8a28c697a6514cd6ccec113bec8ab35c55
Waron <warrenabayon@gmail.com>
2023-10-08 18:04:30 +0800

    finished exercise 5 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    let firstNum = startNumber;
+    let secondNumber = endNumber;
+    let sum = 0;
+
+    // console.log(typeof(firstNum));
+
+   
+
+    if (startNumber < 0 || endNumber < 0 
+                        || typeof(firstNum) !== 'number' 
+                        || typeof(secondNumber) !== 'number') {
+        sum = 'ERROR';
+        
+    } else {
+
+        if (startNumber > endNumber) {
+        firstNum = endNumber;
+        secondNumber = startNumber;
+        }
+
+        for (i = firstNum; i <= secondNumber; i++) {
+            
+            sum += i;
+        
+                            
+        }         
+
+    }
+
+    return sum;
+
 
 };
 
