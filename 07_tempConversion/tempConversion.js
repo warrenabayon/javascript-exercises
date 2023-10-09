c364ff6dfce362745dd9737deaaa47737ff57185
Waron <warrenabayon@gmail.com>
2023-10-10 02:10:49 +0800

    finished exercise 7 cel and fah conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result =  (fahrenheit - 32) * 5/9;
+  return Math.round(result * 10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(celcius) {
+
+  const result =  (celcius * 9/5) + 32 ;
+  return Math.round(result * 10)/10;
+
 };
 
 // Do not edit below this line
