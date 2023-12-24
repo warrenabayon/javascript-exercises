5257db4949bd9798fcf526c78b65e1f6a747a340
Waron <warrenabayon@gmail.com>
2023-12-24 16:04:25 +0800

    done task get the titles

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,17 @@
 const fibonacci = function(num) {
-  let n1 = 0, n2 = 1, sum;
-  
-  for (i = 0 ; i < parseInt(num); i++) {
-    
-    sum = n1 + n2; //get the sum = 1
-    n1 =  n2; // n1 = 1 //so on loop this wil become sum = 1 + 1 = 2
-    n2 = sum; // here n2 becomes 2
-  
-    // console.log(n1);
-  }
-  
+  let n1 = 0 , n2 = 1 ,sum;
 
-  return parseInt(num) < 0 ? 'OOPS' : n1 ;
+  for (i = 0; i < num ; i++) {
+      // so sum gets a new value sum = 1 + 1 = 2
+     sum = n1 + n2; // get the total sum = 1
+     n1 = n2; // n1 becomes 1
+     n2 = sum; // n2 becomes 1
+
+     // then loop to the top
+  }
+ 
+  return num < 0 ? 'OOPS' : n1;
+ 
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,5 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  return books.map((item, index, arr) => item['title']);
 };
 
 // Do not edit below this line
