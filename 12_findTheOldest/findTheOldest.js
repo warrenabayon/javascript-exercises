e6551dbc72f3cc93f2ba487578fe3a21d0e6a928
Waron <warrenabayon@gmail.com>
2023-12-27 03:06:25 +0800

    done with the task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  for (let key in array) { // check the missing and add it
+    'yearOfDeath' in array[key] === false ? array[key].yearOfDeath = new Date().getFullYear() : array;
+  }
+
+  // console.log(array); //check if its added
+  
+  const getAge = array.sort((a , b) => {
+    
+    const n1 = a.yearOfDeath - a.yearOfBirth;
+    const n2 = b.yearOfDeath - b.yearOfBirth;
+    return n1 > n2 ? -1 : 1;
+  });
+
+ return getAge[0];
+
 
 };
 
